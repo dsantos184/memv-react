f53900982a5abfff83e84c0837dd5bf9b04ca6f9
Diogo <diogosantos184@gmail.com>
2019-04-16 14:23:49 -0300

    criacao do container de renovacao e carta de apresentacao

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,44 @@
 import React, {Fragment} from 'react'
 import Titulo from '../../components/Titulo'
 import Carousel from '../../components/Carousel'
+import Botao from '../../components/Botao'
+
 import './style.css'
 
 const Home = (props) =>(
     <Fragment>
         
-        <div className="containerBox">
+        <section className="containerBox">
             <Titulo tituloText="Alguns de Nossos Parceiros"/>
             <span className="subTitulo">Mais de 1.200 instituições cadastradas</span>
-        </div>
+        </section>
         
         <img className="bannerParceiros" src={require('../../img/banner-parceiros.gif')} alt="Mais de 1.200 instituições Parceiras" />
 
-        <div className="containerBox">
+        <section className="containerBox">
             <Titulo tituloText="Instituições em Destaque"/>
             <Carousel />
-        </div>
+        </section>
+
+        <section className="containerCartaRenovacao">
+            
+            <div className="containerRenovacao">
+                <p className="textRenovacao">
+                    Para você que já é nosso aluno do Ens.
+                    Básico, gere o boleto e renove o seu benefício.
+                </p>
+                <Botao  urlTo="/boleto-renovacao" classBtn="btnAzul"  btnText="Quero Renovar Minha Bolsa" />
+            </div>
+
+            <div className="containerCarta">
+                <p className="textCarta">
+                    Clique no botão abaixo para buscar e fazer                    
+                    o download da sua carta de aprensentação.
+                </p>
+                <Botao  urlTo="/boleto-renovacao" classBtn="btnLaranja"  btnText="Gerar Carta de Aprensentação" />
+            </div>
+
+        </section>
 
     </Fragment>
 )
