8eb17dd4c4d37a863fe3a24bbcb257cb571df3d2
Diogo <diogosantos184@gmail.com>
2019-04-17 12:20:43 -0300

    inicio da pagina como funciona - lista e imagem lateral

--- src/pages/ComoFunciona/index.js
+++ src/pages/ComoFunciona/index.js
@@ -1,8 +1,60 @@
 import React, {Fragment} from 'react'
 
+import Titulo from '../../components/Titulo'
+
 const ComoFunciona = (props) =>(
     <Fragment>
-        <h1>Como funciona</h1>
+        
+        <section className="containerTitulo">
+            <Titulo tituloText="Como Funciona" />
+        </section>
+]       
+        <section className="containerComoFunciona">
+            
+            <div className="containerList">
+
+                <ul className="listaComoFunciona">
+                    <li className="itemComoFunciona">
+                        <i className="fa fa-check iconList"></i>
+                        Escolha a região e série pretendida.
+                    </li>
+                    <li className="itemComoFunciona">
+                        <i className="fa fa-check iconList"></i>
+                        Veja lista das instituições e valores disponíveis.
+                    </li>
+                    <li className="itemComoFunciona">
+                        <i className="fa fa-check iconList"></i>
+                        Selecione a instituição de sua preferência.
+                    </li>
+                    <li className="itemComoFunciona">
+                        <i className="fa fa-check iconList"></i>
+                        O programa fará seleção eletrônica e verificará a disponibilidade de vaga.
+                    </li>
+                    <li className="itemComoFunciona">
+                        <i className="fa fa-check iconList"></i>
+                        Após cadastro você será direcionado para tela de pagamento da taxa de adesão.   ( veja valor no menu  “Dúvidas Frequentes” ).
+                    </li>
+                    <li className="itemComoFunciona">
+                        <i className="fa fa-check iconList"></i>
+                        Após confirmação do pagamento enviamos para o e-mail cadastrado a carta de apresentação para ser entregue na instituição no ato da matrícula.
+                    </li>
+                    <li className="itemComoFunciona">
+                        <i className="fa fa-check iconList"></i>
+                        O pagamento da taxa de adesão garante ao aluno o acesso a bolsa de estudos porém será necessário apresentar toda a documentação exigida bem como estar de acordo com o regimento interno da instituição concedente.
+                    </li>
+                </ul>
+            </div>
+
+            <div className="containerImagem">
+                <img 
+                    src={require('../../img/img_box_lateral.jpg')}
+                    className="imgLateral"
+                    alt="Entre Em contato"
+                />
+            </div>
+
+        </section>
+
     </Fragment>
 )
 
